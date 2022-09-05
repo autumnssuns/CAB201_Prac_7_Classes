2f3b75fcd7e612fd8838d98f1c4b7fcbf45f2f5a
MELT User <meltuser@qut.edu.au>
2022-09-05 17:47:08 +1000

    Question 2: MyClock

--- 2. MyClock/MyClock.cs
+++ 2. MyClock/MyClock.cs
@@ -7,7 +7,24 @@ namespace MyClock
     /// </summary>
     class MyClock
     {
-
+        private int hours, minutes, seconds;
+        public int Hours { get { return hours; } }
+        public int Minutes { get { return minutes; } }
+        public int Seconds { get { return seconds; } }
+        public MyClock(int hours, int minutes, int seconds)
+        {
+            if (hours < 0 || minutes < 0 || seconds < 0) Console.WriteLine("Hours, minutes and " + 
+                "seconds cannot be less than 0");
+            else
+            {
+                if (hours >= 24) Console.WriteLine("The hours must be less than 24");
+                else this.hours = hours;
+                if (minutes >= 60) Console.WriteLine("The minutes must be less than 60");
+                else this.minutes = minutes;
+                if (seconds >= 60) Console.WriteLine("The seconds must be less than 60");
+                else this.seconds = seconds;
+            }
+        }
     }
 }
 
---
+++ 2. MyClock/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClock clock = new MyClock(25, 38, 42);
+            Console.WriteLine("{0}:{1}:{2}", clock.Hours, clock.Minutes, clock.Seconds);
+        }
+    }
+}
