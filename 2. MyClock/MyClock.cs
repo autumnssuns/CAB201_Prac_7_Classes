69973f711d8c53826a85b1692142f337fff792ee
MELTUser <MELTUser@MELT-GP-S518N>
2022-09-05 15:53:09 +1000

    Added MyClock

--- 2. MyClock/MyClock.cs
+++ 2. MyClock/MyClock.cs
@@ -5,9 +5,53 @@ namespace MyClock
     /// <summary>
     /// A class to represent a clock.
     /// </summary>
-    class MyClock
+    class MyClock 
     {
+        private int hours;
+        private int minutes;
+        private int seconds;
 
+        public int Hours
+        {
+            get { return hours; }
+        }
+
+        public int Minutes
+        {
+            get { return minutes; }
+        }
+
+        public int Seconds
+        {
+            get { return seconds; }
+        }
+
+        public MyClock(int hours, int minutes, int seconds)
+        {
+            if (hours >= 0 && hours < 24)
+            {
+                this.hours = hours;
+            } else
+            {
+                Console.WriteLine("Invalid hours input");
+            }
+            if (minutes >= 0 && minutes < 60) 
+            {
+                this.minutes = minutes;
+            }
+            else
+            {
+                Console.WriteLine("Invalid minutes input");
+            }
+            if (seconds >= 0 && seconds < 60)
+            {
+                this.seconds = seconds;
+            }
+            else
+            {
+                Console.WriteLine("Invalid seconds input");
+            }
+        }
     }
 }
 
---
+++ 2. MyClock/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClock clock = new MyClock(15, 60, 41);
+
+        }
+    }
+}
