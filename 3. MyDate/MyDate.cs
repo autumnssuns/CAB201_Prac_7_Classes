c8e0a7d24feb38c49a9d1804d6f7dd23ddba9e4c
MELTUser <MELTUser@MELT-GP-S518N>
2022-09-05 16:44:14 +1000

    Question 3 - MyDate

--- 3. MyDate/MyDate.cs
+++ 3. MyDate/MyDate.cs
@@ -7,6 +7,62 @@ namespace MyDate
     /// </summary>
     class MyDate
     {
+        private int year, month, day;
 
+        public int Year
+        {
+            get { return year; }
+        }
+
+        public int Month
+        {
+            get { return month; }
+        }
+
+        public int Day
+        {
+            get { return day; }
+        }
+
+        public MyDate(int year, int month, int day)
+        {
+            if (year <= 0 || month <= 0 || day <= 0)
+            {
+                Console.WriteLine("The year, month or day cannot be negative or 0");
+                return; // Temrinate the constructor
+            }
+            this.year = year;
+            if (month > 12)
+            {
+                Console.WriteLine("The month must be between 1 and 12");
+                return;
+            }
+            this.month = month;
+            bool isLeapYear = (year % 100 != 0 || year % 400 == 0) &&
+               (year % 4 == 0);
+            int dayUpperLimit;
+            switch (month)
+            {
+                case 1 or 3 or 5 or 7 or 8 or 10 or 12:
+                    dayUpperLimit = 31;
+                    break;
+                case 2:
+                    if (isLeapYear) dayUpperLimit = 29;
+                    else dayUpperLimit = 28;
+                    //dayUpperLimit = isLeapYear ? 29 : 28; // Unofficial in CAB201
+                    break;
+                default:
+                    dayUpperLimit = 30;
+                    break;
+            }
+            if (day <= dayUpperLimit)
+            {
+                this.day = day;
+            } else
+            {
+                Console.WriteLine("The day in month {0} cannot be greater than {1}", 
+                    month, dayUpperLimit);
+            }
+        }
     }
 }
---
+++ 3. MyDate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDate date = new MyDate(year: 1904, month: 2, day: 29);
+            Console.WriteLine("The date was {0}/{1}/{2}", date.Day, date.Month, date.Year);
+        }
+
+        /// <summary>
+        /// Method to check if a year is a leap year
+        /// </summary>
+        /// <param name="year">The input year</param>
+        /// <returns>true if the year is a leap year, false otherwise</returns>
+        static bool CheckLeapYear(int year)
+        {
+            return (year % 100 != 0 || year % 400 == 0) && 
+                (year % 4 == 0);
+        }
+    }
+}
